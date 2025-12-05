90367b65ca7894cfe4560bee06aa3e20831f0046
vacponto <62482447+zcaxs@users.noreply.github.com>
2025-12-05 13:25:18 +0000

    Update vite.config.js

--- my-portfolio/vite.config.js
+++ my-portfolio/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/zcaxs.github.io/',
+  base: 'base: process.env.NODE_ENV === 'production' ? '/' : '/'',
   plugins: [react()],
 })
