5b80ac69b50efc84a3dbeb66a2babef2fd555122
vacponto <62482447+zcaxs@users.noreply.github.com>
2025-12-05 15:13:31 +0000

    reworked Email

--- my-portfolio/src/pages/Home.jsx
+++ my-portfolio/src/pages/Home.jsx
@@ -30,7 +30,7 @@ export default function Home() {
           
           {/* Social Icons Only */}
           <div className="social-icons">
-            <a href={`mailto:${m.email}`} className="social-icon" title="Email">
+            <a href={`https://mail.google.com/mail/?view=cm&fs=1&to=${m.email}`} className="social-icon" title="Email">
               <svg className="icon" viewBox="0 0 24 24">
                 <path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"/>
               </svg>
